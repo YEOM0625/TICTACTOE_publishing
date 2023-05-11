b7a2596884fe666c8c1e8ca680a5042969201a9a
YEOM0625 <109888011+YEOM0625@users.noreply.github.com>
2023-05-11 17:50:34 +0900

    Board.js 수정

--- hooks/src/components/Square.js
+++ hooks/src/components/Square.js
@@ -4,31 +4,18 @@ import "./Square.css";
 
 
 export default class Square extends React.Component{
-    // constructor(props){
-    //     super(props);
-    //     this.state = {
-    //         value: null,
-    //     };
-    // }
-    // State 생성 (무언가를 "기억하기" 위해 component는 state를 사용.)
-    // 동기화를 위해 부모에 옮겨줌(Board.js)
+  
     render(){
         return(
-            // <button className="square"
-            //     onClick={() => {console.log('click')}}>
-            
-            // State 변경(클릭 시, value 값을 X로 바꾸겠다.)
+
             <button className="square"
             onClick={() => {this.props.onClick()}}>
                 
-                {/* {this.props.value} */}
+               
                 {this.props.value} 
             </button>
             )
     }// state 이용(this.state.value)
 }
 
-// Square의 render 함수 내부의 this.state.value를 this.props.value로 바꿔줍니다.
-// Square의 render 함수 내부의 this.setState()를 this.props.onClick()으로 바꿔줍니다..
-// Square는 게임의 상태를 유지할 필요가 없기 때문에 constructor를 지워줍니다.
 
