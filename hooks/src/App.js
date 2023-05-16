acb8ea0b0c8ae5ddf306db7dd64837135b989afb
YEOM0625 <109888011+YEOM0625@users.noreply.github.com>
2023-05-16 19:03:34 +0900

    앱 스타일 변경

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -68,7 +68,7 @@ function App() {
     'Go to game start';
     return(
       <li key ={move}> 
-        <button onClick={()=>jumpTo(move)}>{desc}</button>
+        <button className='move-button' onClick={()=>jumpTo(move)}>{desc}</button>
       </li>
     )// 리스트들에게 고유한 key 값을 주면 에러(unique "key" prop.)가 해결됨. move는 history 배열의 index값.
 })
@@ -85,7 +85,7 @@ function App() {
       </div>
       <div className="game-info">
       <div className='status'>{status}</div>
-      <ol>{moves}</ol>
+      <ol style={{listStyle: 'none'}}>{moves}</ol>
       </div>
       
     </div>
